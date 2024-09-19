eb294e44e6ba0e101f1e10cffd5b205c3e30e7d7
watchoutfreedom <watchoutfreedom@gmail.com>
2024-09-19 14:32:47 +0200

    a

---
+++ components/message.php
@@ -0,0 +1,9 @@
+  <div class="last__menu">
+    <a href="#portada">Portada</a>
+		<a href="#ponentes">Ponentes</a>
+		<a href="#entradas">Entradas</a>
+		<a href="#programa">Programa</a>
+		<a href="#sponsor">Sponsors</a>
+		<a href="#faq">FAQs</a>
+		<a href="#2023">Evento23</a>
+  </div>
