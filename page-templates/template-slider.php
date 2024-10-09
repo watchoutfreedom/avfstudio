9d3e3cb2fdef233a35024082630bb5d2e199c761
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-09 18:59:54 +0200

    d

--- page-templates/template-slider.php
+++ page-templates/template-slider.php
@@ -174,7 +174,7 @@ get_header();
                 wp_reset_postdata();
             }
             ?>
-            <div class="contact-message">Contact </div>
+            <div class="contact-message">Contact 3</div>
         </div>
         <?php
     }
