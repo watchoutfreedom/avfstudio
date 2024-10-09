afb77a71b5236f9396da91bf884ab9b5ad9dd83d
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-09 18:57:54 +0200

    a

--- extra files/page-templates/template-slider.php
+++ extra files/page-templates/template-slider.php
@@ -241,7 +241,7 @@ get_header();
             wp_reset_postdata();
         }
         ?>
-        <div class="contact-message">Contact</div>
+        <div class="contact-message">Contact 3</div>
     </div>
 </div>
 
