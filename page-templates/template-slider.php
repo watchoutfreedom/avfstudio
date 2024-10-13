60d79ee3b36c236748a324351529a20c91d9af0b
watchoutfreedom <watchoutfreedom@gmail.com>
2024-10-14 00:16:51 +0200

    w

--- page-templates/template-slider.php
+++ page-templates/template-slider.php
@@ -11,7 +11,7 @@ get_header();
     $sections = array(
         array(
             'slug' => 'home',
-            'title' => 'Welcome',
+            'title' => '',
             'subtitle' => '',
         ),
         array(
